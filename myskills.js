0425fab83e035f66d83217e89db76936e1ed2f1a
mario.santella <mario.santella@3rdplace.com>
2023-01-18 10:24:06 +0100

    create json structure array of skills

---
+++ myskills.js
@@ -0,0 +1,12 @@
+let skills = [
+     {"skill": "Python", "score": 8},
+     {"skill": "Javascript", "score": 8},
+     {"skill": "Laravel", "score": 6},
+     {"skill": "PHP", "score": 7},
+     {"skill": "MongoDB", "score": 7},
+     {"skill": "MySQL", "score": 6},
+     {"skill": "GraphQL", "score": 5},
+     {"skill": "Cloud Computing", "score": 6},
+   ];
+
+console.log(skills);
